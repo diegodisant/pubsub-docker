69b9f521e055e65fd57211f908eb641193251459
diegodisant <developerdiego0@gmail.com>
2020-10-03 13:44:30 -0500

    fix: publisher run with google pubsub 2.0.0 for py

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,16 @@
-FROM google/cloud-sdk:272.0.0
+FROM google/cloud-sdk:latest
 
-RUN pip install grpcio
-RUN pip install google-cloud-pubsub==1.6.1
+COPY ./requirements.txt .
+
+RUN pip install --upgrade pip
+RUN pip install -r requirements.txt
+
 RUN mkdir -p /root/bin
 
-COPY start-pubsub.sh /root/bin
-COPY pubsub-client.py /root/bin
+COPY ./pubsub-client.py /root/bin
+COPY ./start-pubsub.sh /root/bin
+
+RUN chmod +x /root/bin/start-pubsub.sh
 
 ENV PUBSUB_EMULATOR_HOST=localhost:8432
 
--- start-pubsub.sh
+++ start-pubsub.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ -z "${PUBSUB_PROJECT_ID}" ]]; then
-  echo "No PUBSUB_PROJECT_ID supplied, setting default of docker-gcp-project"
+  echo "[error] No PUBSUB_PROJECT_ID supplied, setting default to docker-gcp-project"
 
   export PUBSUB_PROJECT_ID=docker-gcp-project
 fi
@@ -10,18 +10,18 @@ gcloud beta emulators pubsub start --host-port=0.0.0.0:8432 &
 
 PUBSUB_PID=$!
 
-if [[ -z "${PUBSUB_CONFIG}" ]]; then
-  echo "No PUBSUB_CONFIG supplied, no additional topics or subscriptions will be created"
+if [[ -z "${PUBSUB_TOPIC_ID}" ]]; then
+  echo "[error] No PUBSUB_TOPIC_ID supplied"
 else
-  echo "Creating topics and subscriptions"
+  echo "[pubsub] Creating topics and subscriptions"
 
-  python /root/bin/pubsub-client.py create ${PUBSUB_PROJECT_ID} "${PUBSUB_CONFIG}"
+  python3 /root/bin/pubsub-client.py ${PUBSUB_PROJECT_ID} create ${PUBSUB_TOPIC_ID}
 
   if [ $? -eq 1 ]; then
     exit 1
   fi
 fi
 
-echo "[pubsub] Ready for process..."
+echo "[pubsub] Ready for process [${PUBSUB_TOPIC_ID}] topic..."
 
 wait ${PUBSUB_PID}
